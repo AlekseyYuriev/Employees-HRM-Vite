87cfcff3aa702d2f340831553ab919db897c1528
Aleksey Yuriev <alex11-90@yandex.by>
2025-07-02 18:06:29 +0200

    fix: update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,21 @@
 // vitest.config.ts
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vitest/config";
+import { coverageConfigDefaults } from "vitest/config";
 
 export default defineConfig({
   test: {
     environment: "jsdom",
     coverage: {
-      provider: "v8",
       reporter: ["text", "json", "html"],
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        "coverage/**",
+        "dist/**",
+        "**/node_modules/**",
+        "**/*.d.ts",
+        "**/*.config.{js,cjs,mjs,ts,cts,mts}",
+      ],
     },
     include: ["**/*.spec.ts"],
   },
